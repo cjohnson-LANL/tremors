eb7eeb8c797d5cf3324aba1be7fb2789da5a2474
Richard Alfaro <128748499+rad-lanl@users.noreply.github.com>
2026-05-04 17:08:50 -0600

    Update Example-ollama-event-waveform-data.py

--- notebooks/Example-ollama-event-waveform-data.py
+++ notebooks/Example-ollama-event-waveform-data.py
@@ -12,7 +12,9 @@ llm = ChatOpenAI(
 )
 
 inputs = {
- "query": "Get continuous waveforms between latitudes 33 and 34, and longitudes -116 and -117 for February 1st to February 3rd, 2016. Look for BH* channels on the CI network. Do not use directory dates, use directory stats, and download response.",
+    "query": "Find 2 unique events in Northern California from 2016 with magnitude > 5.0. Get waveforms and plot them.",
+    # "query": "Get waveforms and plot them for event between 2025-10-10 12:47:00 and  2025-10-10 12:49:00 near latitude 35.921, longitude -87.658. Download data from all stations and channels within 3 degrees.",
+    # "query": "Find thee largest Earthquake to occur in Japan after 2009. Get Teleseismic distance waveforms and plot them.",
     "output_dir": "./temp"
 }
 
